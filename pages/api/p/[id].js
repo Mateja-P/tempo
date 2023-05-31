f83aaa6e9f5fa73d2bc0ffc77b0461c963b1d8f3
Mateja <mateja@alideda.rs>
2023-05-31 10:34:44 +0200

    tempo

--- pages/api/p/[id].js
+++ pages/api/p/[id].js
@@ -47,7 +47,22 @@ export default function id(req, res) {
     e.id === id;
   });
 
-  res.send(sendData[0]);
+  // res.send(sendData);
+  res.send({
+    categories_id: 2,
+    id: 2,
+    information:
+      'Solja je napravljena od nerdjajuceg celika. Najmanja kolicina je 1 komad',
+    model: 'metalCup.glb',
+    modelName: 'Metalna solja',
+    mostPopular: 'true',
+    price: '900',
+    printPrice: '160',
+    productsImage: '{"properties": ["nepalsubli1.png", "nepalsubli2.png"]}',
+    promoId: 'Nepal subli',
+    properties: '{"properties": {"sizes": "7.5 x 8.9 cm", "gender": "null"}}',
+  });
+  console.log(sendData);
 
   //   const q = `SELECT defaultProducts.*, subproducts.id as subId, subproducts.modelName, subproducts.model, subproducts.price, subproducts.discountPrice, subproducts.defaultProducts_id, subproducts.information, productsproperties.properties, faces.faces FROM defaultProducts inner join subproducts on defaultProducts.id = subproducts.defaultProducts_id inner join productsproperties on subproducts.id = productsproperties.subProducts_id inner join faces on subproducts.id = faces.subProducts_id WHERE subproducts.id = ${id}`;
   // const q = `SELECT products.*, properties.properties FROM products INNER JOIN properties ON properties.products_id = products.id WHERE products.id = ${id}`;
