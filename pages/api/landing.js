47c88d72976535ab60a1c83337a9c8179ebab018
Mateja <mateja@alideda.rs>
2023-05-31 10:18:08 +0200

    tempo

--- pages/api/landing.js
+++ pages/api/landing.js
@@ -9,10 +9,37 @@ const db = mysql.createConnection({
 });
 
 export default function landing(req, res) {
-  res.send('Nice');
-  const q = 'SELECT * FROM products';
+  res.send([
+    {
+      categories_id: 1,
+      id: 1,
+      information: 'Majca je 100% od pamuka. Najmanja kolicina je 1 komad',
+      model: 'shirtmodel.glb',
+      modelName: 'Majca kratkih rukava',
+      mostPopular: 'true',
+      price: '1450',
+      printPrice: '160',
+      productsImage:
+        '{"properties": ["premium1801.png", "premium1802.png", "premium1803.png"]}',
+      promoId: 'Premium',
+    },
+    {
+      categories_id: 2,
+      id: 2,
+      information:
+        'Solja je napravljena od nerdjajuceg celika. Najmanja kolicina je 1 komad',
+      model: 'metalCup.glb',
+      modelName: 'Metalna solja',
+      mostPopular: 'true',
+      price: '900',
+      printPrice: '160',
+      productsImage: '{"properties": ["nepalsubli1.png", "nepalsubli2.png"]}',
+      promoId: 'Nepal subli',
+    },
+  ]);
+  // const q = 'SELECT * FROM products';
 
-  db.query(q, (error, data) => {
-    return res.json(data);
-  });
+  // db.query(q, (error, data) => {
+  //   return res.json(data);
+  // });
 }
